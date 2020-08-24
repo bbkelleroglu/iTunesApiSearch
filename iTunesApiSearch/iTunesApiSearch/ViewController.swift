7cfae2ae99231591f567b165ddbd4d1e25ab1546
bkelleroglu <burakkelleroglu@ug.bilkent.edu.tr>
2020-08-24 15:58:00 +0300

    initial commit

---
+++ iTunesApiSearch/iTunesApiSearch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iTunesApiSearch
+//
+//  Created by Burak Kelleroğlu on 24.08.2020.
+//  Copyright © 2020 Burak Kelleroglu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
