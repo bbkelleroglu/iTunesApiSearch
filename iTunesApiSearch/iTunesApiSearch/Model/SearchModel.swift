aa0af2916e2f2ac57566c2a28fc4664e3c4e3c66
bkelleroglu <burakkelleroglu@ug.bilkent.edu.tr>
2020-08-28 01:28:19 +0300

    model: define searchModel to use as a datasource *implement searchModel to place retrieved data and decode

---
+++ iTunesApiSearch/iTunesApiSearch/Model/SearchModel.swift
@@ -0,0 +1,23 @@
+//
+//  SearchModel.swift
+//  iTunesApiSearch
+//
+//  Created by Burak Kelleroğlu on 28.08.2020.
+//  Copyright © 2020 Burak Kelleroglu. All rights reserved.
+//
+
+import Foundation
+struct SearchResults: Decodable {
+    let resultCount: Int
+    let results: [SearchModel]
+}
+
+// MARK: - Result
+struct SearchModel: Decodable {
+    let trackID: Int
+    let artistName: String
+    let collectionName: String
+    let trackName: String
+    let collectionArtistName: String
+    let artworkUrl100: String
+}
